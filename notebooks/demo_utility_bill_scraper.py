bb7d807413feffe733c3bec7179adc6883a0f491
Ryan Fobel <ryan@fobel.net>
2019-01-21 21:10:29 -0500

    PEP8 style fixes

--- notebooks/demo_utility_bill_scraper.py
+++ notebooks/demo_utility_bill_scraper.py
@@ -15,7 +15,8 @@
 
 # # Introduction
 #
-# This notebook demonstrates scraping of data from a series of utility bills (pdfs). Currently, this library supports:
+# This notebook demonstrates scraping of data from a series of utility bills
+# (pdfs). Currently, this library supports:
 #
 # * [Kitchener Utilities (gas & water)](https://www.kitchenerutilities.ca)
 # * [Kitchener-Wilmot Hydro (electricity)](https://www.kwhydro.on.ca)
@@ -24,16 +25,16 @@
 # %load_ext autoreload
 # %autoreload 2
 
-# add parent directory to python path
-import sys
-sys.path.insert(0,'..')
-
 import subprocess
 import os
 from glob import glob
 
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
+
+# Add parent directory to python path.
+import sys
+sys.path.insert(0, '..')
 from utility_bill_scraper import process_pdf, convert_data_to_df
 
 # %matplotlib inline
@@ -42,19 +43,19 @@ rcParams.update({
     'figure.figsize': (12, 6)
 })
 
-
-# Bills are stored in the tests directory (separate subdirectory for each utility)
 bills_list = glob(os.path.join(os.path.join('..', 'tests', '*'), '*.pdf'))
 
-# scrape pdfs
 data = []
 
 print('Scrape data from the following pdf files:')
 for pdf_file in bills_list:
     print('  %s' % pdf_file)
-    data.append(process_pdf(pdf_file))
-    
-# convert the list of dictionaries into a dictionary of data frames (one for each utility in the dataset)
+    result = process_pdf(pdf_file)
+    if result:
+        data.append(result)
+
+# Convert the list of dictionaries into a dictionary of data frames (one for
+# each utility in the dataset).
 df = convert_data_to_df(data)
 # -
 
@@ -63,25 +64,39 @@ df = convert_data_to_df(data)
 # +
 df_gas = df['Kitchener Utilities']
 
-plt.figure(); df_gas['Gas & Water Charges'].plot(); plt.title('Gas & Water Charges'); plt.ylabel('\$')
-plt.figure(); df_gas['Gas Consumption'].plot(); plt.title('Gas Consumption'); plt.ylabel('m$^3$')
-plt.figure(); df_gas['Water Consumption'].plot(); plt.title('Water Consumption'); plt.ylabel('m$^3$')
+plt.figure()
+df_gas['Gas & Water Charges'].plot()
+plt.title('Gas & Water Charges')
+plt.ylabel('\$')
+
+plt.figure()
+df_gas['Gas Consumption'].plot()
+plt.title('Gas Consumption')
+plt.ylabel('m$^3$')
+
+plt.figure()
+df_gas['Water Consumption'].plot()
+plt.title('Water Consumption')
+plt.ylabel('m$^3$')
 
 df_gas.to_csv('Kitchener Utilities data.csv')
 
 # Natural gas emission factor
 # 119.58 lbs CO2/1000 cubic feet of natural gas
-# 1.915 kg CO2/1 m^3 natural gas [119.58 lbs * (1 kg / 2.204623 lbs) * (1 ft^3 / (0.0254 * 12)**3 m^3) / 1000]
-kgCO2_per_cubic_meter = 119.58 * (1 / 2.204623) * (1 / (0.0254 * 12)**3) / 1000 # kg CO2/1 m^3 natural gas
+# 1.915 kg CO2/1 m^3 natural gas [119.58 lbs * (1 kg / 2.204623 lbs) *
+#   (1 ft^3 / (0.0254 * 12)**3 m^3) / 1000]
+kgCO2_per_cubic_meter = 119.58 * (1 / 2.204623) * \
+    (1 / (0.0254 * 12)**3) / 1000  # kg CO2/1 m^3 natural gas
 
-gas_variable_rate = 0.068371 + 0.135000 # $ / m^3
+gas_variable_rate = 0.068371 + 0.135000  # $ / m^3
 
 # Natural gas energy density
 # 1,037 Btu / ft^3 (https://www.eia.gov/tools/faqs/faq.php?id=45&t=8)
-# Energy per m^3: 1,037 Btu / ft^3 * 1055.1 J / 1 Btu * 1 ft^3 / (0.0254 * 12)**3 m^3
+# Energy per m^3: 1,037 Btu / ft^3 * 1055.1 J / 1 Btu * 1 ft^3 /
+#   (0.0254 * 12)**3 m^3
 #   37 MJ/m3 (https://hypertextbook.com/facts/2002/JanyTran.shtml)
 
-joules_per_cubic_meter = 1037 * 1055.1 / (0.0254 * 12)**3 # J / m^3
+joules_per_cubic_meter = 1037 * 1055.1 / (0.0254 * 12)**3  # J / m^3
 kwh_per_joule = 1.0 / (60 * 60 * 1000)
 kwh_per_cubic_meter = joules_per_cubic_meter * kwh_per_joule
 
@@ -92,13 +107,19 @@ energy_use_kwh = df_gas['Gas Consumption'].sum() * kwh_per_cubic_meter
 furnace_efficiency = 0.95
 
 print('annual gas usage: %.1f m^3' % (df_gas['Gas Consumption'].sum()))
-print('annual heating energy usage: %.1f GJ (%.1f kWh)' % (energy_use_joules / 1e9, energy_use_kwh))
-print('carbon intensity (heating): %d g CO2 / kWh' % (kgCO2_per_cubic_meter / kwh_per_cubic_meter * 1000))
-print('heating energy per m^3 natural gas (%d%% efficient furnace): %.2f kWh / m^3' % (
-    100 * furnace_efficiency, furnace_efficiency * kwh_per_cubic_meter))
-print('gas heating cost: $%.3f / kWh' % (gas_variable_rate / kwh_per_cubic_meter))
-print('annual CO2 emissions from natural gas: %.1f kg' % (df_gas['Gas Consumption'].sum() * kgCO2_per_cubic_meter))
-print('annual cost for natural gas: $%.2f' % (df_gas['Gas Consumption'].sum() * gas_variable_rate))
+print('annual heating energy usage: %.1f GJ (%.1f kWh)' % (
+    energy_use_joules / 1e9, energy_use_kwh))
+print('carbon intensity (heating): %d g CO2 / kWh' % (
+    kgCO2_per_cubic_meter / kwh_per_cubic_meter * 1000))
+print('heating energy per m^3 natural gas (%d%% efficient furnace):'
+      ' %.2f kWh / m^3' % (
+          100 * furnace_efficiency, furnace_efficiency * kwh_per_cubic_meter))
+print('gas heating cost: $%.3f / kWh' % (
+    gas_variable_rate / kwh_per_cubic_meter))
+print('annual CO2 emissions from natural gas: %.1f kg' % (
+    df_gas['Gas Consumption'].sum() * kgCO2_per_cubic_meter))
+print('annual cost for natural gas: $%.2f' % (
+    df_gas['Gas Consumption'].sum() * gas_variable_rate))
 
 df_gas
 # -
@@ -108,12 +129,14 @@ df_gas
 # +
 df_electricity = df['Kitchener-Wilmot Hydro']
 
-plt.figure();
-df_electricity['Off Peak Consumption'].plot();
-df_electricity['Mid Peak Consumption'].plot();
-df_electricity['On Peak Consumption'].plot();
+plt.figure()
+df_electricity['Off Peak Consumption'].plot()
+df_electricity['Mid Peak Consumption'].plot()
+df_electricity['On Peak Consumption'].plot()
 df_electricity['Total Consumption'].plot()
-plt.ylim((0, None)); plt.title('Monthly Electricity Consumption'); plt.ylabel('kWh');
+plt.ylim((0, None))
+plt.title('Monthly Electricity Consumption')
+plt.ylabel('kWh')
 plt.legend(['Off Peak', 'Mid Peak', 'On Peak', 'Total'])
 
 df_electricity.to_csv('Kitchener-Wilmot Hydro data.csv')
@@ -125,29 +148,36 @@ df_electricity.to_csv('Kitchener-Wilmot Hydro data.csv')
 #   https://www.opg.com/darlington-refurbishment/Documents/IntrinsikReport_GHG_OntarioPower.pdf
 
 cabron_intensity_kgCO2_per_kwh = .077
-print('annual electricity usage: %.1f kWh' % (df_electricity['Total Consumption'].sum()))
+print('annual electricity usage: %.1f kWh' % (
+    df_electricity['Total Consumption'].sum()))
 print('annual electricity cost: $%.2f' % (df_electricity['Amount Due'].sum()))
 print('annual CO2 emissions from electricity: %.2f kg' % (
-    df_electricity['Total Consumption'].sum() * cabron_intensity_kgCO2_per_kwh))
+    df_electricity['Total Consumption'].sum() *
+    cabron_intensity_kgCO2_per_kwh))
 
 df_electricity
 # -
 
 # # Monthly usage and offsetting
 #
-# Calculate average monthly usage and costs for offsetting with [Bullfrog Power](https://www.bullfrogpower.com).
+# Calculate average monthly usage and costs for offsetting with
+# [Bullfrog Power](https://www.bullfrogpower.com).
 
 # +
-print('monthly electricity usage: %.1f kWh (avg), %.1f kWh (max)' % (df_electricity['Total Consumption'].mean(),
-                                                                     df_electricity['Total Consumption'].max()))
+print('monthly electricity usage: %.1f kWh (avg), %.1f kWh (max)' % (
+    df_electricity['Total Consumption'].mean(),
+    df_electricity['Total Consumption'].max()))
 
-print('monthly gas usage: %.1f m^3 (avg), %.1f m^3 (max)' % (df_gas['Gas Consumption'].mean(),
-                                                             df_gas['Gas Consumption'].max()))
+print('monthly gas usage: %.1f m^3 (avg), %.1f m^3 (max)' % (
+    df_gas['Gas Consumption'].mean(), df_gas['Gas Consumption'].max()))
 
-print('\nmonthly electricity offset: $%.2f' % (df_electricity['Total Consumption'].mean() * 0.025))
-print('monthly gas offset: $%.2f' % (df_gas['Gas Consumption'].mean() * 0.15))
-print('total monthly offset: $%.2f' % (df_electricity['Total Consumption'].mean() * 0.025 + 
-                                       df_gas['Gas Consumption'].mean() * 0.15))
+print('\nmonthly electricity offset: $%.2f' % (
+    df_electricity['Total Consumption'].mean() * 0.025))
+print('monthly gas offset: $%.2f' % (
+    df_gas['Gas Consumption'].mean() * 0.15))
+print('total monthly offset: $%.2f' % (
+    df_electricity['Total Consumption'].mean() * 0.025 +
+    df_gas['Gas Consumption'].mean() * 0.15))
 # -
 
 
--- utility_bill_scraper/__init__.py
+++ utility_bill_scraper/__init__.py
@@ -1,38 +1,44 @@
 import os
 import subprocess
 
-import pandas as pd
 import arrow
+import pandas as pd
 from bs4 import BeautifulSoup
+
 
 def is_number(s):
     """Returns True if string is a number."""
-    return s.replace('.','',1).isdigit()
+    return s.replace('.', '', 1).isdigit()
 
 
 def format_fields(input_list):
-    fields = [x.rstrip().strip(':') for x in input_list if x.find('</br>') == -1]
+    fields = [x.rstrip().strip(':') for x in input_list
+              if x.find('</br>') == -1]
     return [float(x) if is_number(x) else x for x in fields]
 
 
 def is_kitchener_utilities_bill(soup):
     """Check if this is a Kitchener Utilities bill."""
     def find_kitchener_utilities(tag):
-        return tag.name == u'span' and tag.decode().find('Supplier: KITCHENER UTILITIES') >=0
+        return tag.name == u'span' and tag.decode().find(
+            'Supplier: KITCHENER UTILITIES') >= 0
     return len(soup.find_all(find_kitchener_utilities)) > 0
 
 
 def is_kitchener_wilmot_hydro_bill(soup):
     """Check if this is a Kitchener-Wilmot Hydro bill."""
     def find_kitchener_wilmot_hydro(tag):
-        return tag.name == u'span' and tag.decode().find('KITCHENER-WILMOT HYDRO INC') >=0
+        return tag.name == u'span' and tag.decode().find(
+            'KITCHENER-WILMOT HYDRO INC') >= 0
     return len(soup.find_all(find_kitchener_wilmot_hydro)) > 0
+
 
 def process_pdf(pdf_file, rename=False, keep_html=False):
     basename, ext = os.path.splitext(pdf_file)
     basepath = os.path.dirname(pdf_file)
     html_file = basename + '.html'
-    subprocess.check_output(['python', '%CONDA_PREFIX%\Scripts\pdf2txt.py', '-o%s' % html_file, pdf_file], shell=True)
+    subprocess.check_output(['python', '%CONDA_PREFIX%\Scripts\pdf2txt.py',
+                             '-o%s' % html_file, pdf_file], shell=True)
 
     with open(html_file, 'r') as f:
         soup = BeautifulSoup(f, 'html.parser')
@@ -48,7 +54,7 @@ def process_pdf(pdf_file, rename=False, keep_html=False):
 
     if (is_kitchener_utilities_bill(soup)):
         import kitchener_utilities as ku
-        
+
         summary = ku.get_summary(soup)
 
         if 'Pre-authorized Withdrawal' in summary.keys():
@@ -59,30 +65,31 @@ def process_pdf(pdf_file, rename=False, keep_html=False):
             print("Couldn't find amount due!")
             amount_due = None
 
-        new_name = '%s-Kitchener utilities-$%s.pdf' % (arrow.get(summary['Issue Date'], 'MMM DD YYYY').format('YYYY-MM-DD'),
-            amount_due)
+        new_name = '%s-Kitchener utilities-$%s.pdf' % (
+            arrow.get(summary['Issue Date'],
+                      'MMM DD YYYY').format('YYYY-MM-DD'), amount_due)
 
-        result = {  'name': ku.get_name(),
-                    'summary': summary,
-                    'water consumption': ku.get_water_consumption(soup),
-                    #'water and sewer charges': ku.get_water_and_sewer_charges(soup),
-                    'gas consumption': ku.get_gas_consumption(soup),
-                    'gas charges': ku.get_gas_charges(soup),
-                    'gas rates': ku.get_gas_rates(soup)
-                    }
+        result = {'name': ku.get_name(),
+                  'summary': summary,
+                  'water consumption': ku.get_water_consumption(soup),
+                  # 'water and sewer charges':
+                  # ku.get_water_and_sewer_charges(soup),
+                  'gas consumption': ku.get_gas_consumption(soup),
+                  'gas charges': ku.get_gas_charges(soup),
+                  'gas rates': ku.get_gas_rates(soup)}
     elif (is_kitchener_wilmot_hydro_bill(soup)):
         import kitchener_wilmot_hydro as kwh
 
         date = kwh.get_billing_date(soup)
         amount_due = kwh.get_amount_due(soup)
         new_name = '%s-Kitchener-Wilmot hydro-$%s.pdf' % (date, amount_due)
-        
-        result = {  'name': kwh.get_name(),
-                    'date': date,
-                    'amount due': amount_due,
-                    'electricity rates': kwh.get_electricity_rates(soup),
-                    'electricity consumption': kwh.get_electricity_consumption(soup)
-                    }
+
+        result = {'name': kwh.get_name(),
+                  'date': date,
+                  'amount due': amount_due,
+                  'electricity rates': kwh.get_electricity_rates(soup),
+                  'electricity consumption':
+                  kwh.get_electricity_consumption(soup)}
     else:
         print("Unrecognized bill type.")
 
@@ -108,5 +115,5 @@ def convert_data_to_df(data):
             print('Unknown name.')
             continue
         result[x['name']] = result[x['name']].append(df)
-        
+
     return result
