1b57620b6b259ce59ccb18133e3f72868fea7df5
Ryan Fobel <ryan@fobel.net>
2019-01-21 21:10:29 -0500

    PEP8 style fixes

--- utility_bill_scraper/kitchener_wilmot_hydro.py
+++ utility_bill_scraper/kitchener_wilmot_hydro.py
@@ -1,19 +1,22 @@
 import re
 
 import arrow
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-from utility_bill_scraper import is_number, format_fields
+from utility_bill_scraper import format_fields, is_number
+
 
 def get_name():
     return 'Kitchener-Wilmot Hydro'
 
+
 def get_electricity_consumption(soup):
     def find_kWh(tag):
-        return tag.name == u'span' and (tag.decode().find('kWh Off Peak') >= 0 or
-                                        tag.decode().find('kWh Mid Peak') >= 0 or
-                                        tag.decode().find('kWh On Peak') >= 0)
+        return tag.name == u'span' and (
+            tag.decode().find('kWh Off Peak') >= 0 or
+            tag.decode().find('kWh Mid Peak') >= 0 or
+            tag.decode().find('kWh On Peak') >= 0)
     fields = []
     for x in soup.find_all(find_kWh):
         fields += format_fields(x)
@@ -32,14 +35,20 @@ def get_electricity_consumption(soup):
 
     return data
 
+
 def get_billing_date(soup):
     def find_billing(tag):
         return tag.name == u'div' and tag.decode().find('BILLING DATE') >= 0
 
-    match = re.search('([A-Z]+)\s+(\d+)\s+(\d+)', format_fields(soup.find_all(find_billing)[0].next_sibling.next_sibling.span.contents)[0])
+    match = re.search('([A-Z]+)\s+(\d+)\s+(\d+)',
+                      format_fields(soup.find_all(find_billing)[0].
+                                    next_sibling.next_sibling.span.
+                                    contents)[0])
     month, day, year = match.groups()
 
-    return arrow.get("%s %s %s" % (month, day, year), 'MMM DD YYYY').date().isoformat()
+    return arrow.get("%s %s %s" % (month, day, year),
+                     'MMM DD YYYY').date().isoformat()
+
 
 def get_electricity_rates(soup):
     def find_kWhOffPeak(tag):
@@ -60,6 +69,7 @@ def get_electricity_rates(soup):
             break
 
     return dict(zip(['off peak', 'on peak', 'mid peak'], rates))
+
 
 def get_amount_due(soup):
     def find_new_charges(tag):
@@ -84,7 +94,8 @@ def get_amount_due(soup):
         match = re.search('top:(?P<top>\d+)px', tag.decode())
         distance.append(abs(float(match.groups()[0]) - top))
 
-    amount_due = format_fields(tags[np.nonzero(distance == np.min(distance))[0][0]].span)[0]
+    amount_due = format_fields(
+        tags[np.nonzero(distance == np.min(distance))[0][0]].span)[0]
 
     index = str(amount_due).find('CR')
     if index >= 0:
@@ -92,17 +103,24 @@ def get_amount_due(soup):
 
     return amount_due
 
+
 def convert_data_to_df(data):
     df = pd.DataFrame({'Date': [arrow.get(x['date']).date() for x in data],
                        'Amount Due': [x['amount due'] for x in data],
-                       'Off Peak Consumption': [x['electricity consumption']['off peak'] for x in data],
-                       'Mid Peak Consumption': [x['electricity consumption']['mid peak'] for x in data],
-                       'On Peak Consumption': [x['electricity consumption']['on peak'] for x in data],
-                       'Off Peak Rate': [x['electricity rates']['off peak'] for x in data],
-                       'Mid Peak Rate': [x['electricity rates']['mid peak'] for x in data],
-                       'On Peak Rate': [x['electricity rates']['on peak'] for x in data]
-                      })
+                       'Off Peak Consumption': [x['electricity consumption']
+                                                ['off peak'] for x in data],
+                       'Mid Peak Consumption': [x['electricity consumption']
+                                                ['mid peak'] for x in data],
+                       'On Peak Consumption': [x['electricity consumption']
+                                               ['on peak'] for x in data],
+                       'Off Peak Rate': [x['electricity rates']
+                                         ['off peak'] for x in data],
+                       'Mid Peak Rate': [x['electricity rates']
+                                         ['mid peak'] for x in data],
+                       'On Peak Rate': [x['electricity rates']
+                                        ['on peak'] for x in data]})
     df = df.set_index('Date')
 
-    df['Total Consumption'] = df['Off Peak Consumption'] + df['Mid Peak Consumption'] + df['On Peak Consumption']
+    df['Total Consumption'] = df['Off Peak Consumption'] + \
+        df['Mid Peak Consumption'] + df['On Peak Consumption']
     return df
