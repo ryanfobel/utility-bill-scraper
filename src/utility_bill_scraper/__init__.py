10cb8b3f5508aea56a3b5f333a34488fc572c10a
Ryan Fobel <ryan@fobel.net>
2021-12-01 11:16:55 -0500

    Add chrome options for compatibility with colab

--- src/utility_bill_scraper/__init__.py
+++ src/utility_bill_scraper/__init__.py
@@ -286,6 +286,8 @@ class UtilityAPI:
                 options.add_argument("--window-size=1920,1080")
                 options.add_argument("--headless")
                 options.add_argument("--disable-gpu")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
                 options.add_argument(
                     "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
                 )
